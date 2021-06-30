d94616fb2e2e51b59ad8eaa463f43f39b2f80120
Radoslav <Rado@rado.local>
2021-06-30 13:27:18 +0200

    ready for 1st milestone

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+function myFunction() {
+    var x = document.getElementById("floatingPassword");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
