fca3e4e8f837b0ab3775de6a01f8e71ed8830f8e
mattesho <esho_matti@hotmail.com>
2022-01-28 10:35:27 +0100

    adding products workin now

--- js/product-logic.js
+++ js/product-logic.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     $('#add_new_product_btn').click(AddNewProduct);
 
     function AddNewProduct() {
-        const title = $('#new_product_title').val();
-        const desc = $('#new_product_desc').val();
+        const name = $('#new_product_title').val();
+        const description = $('#new_product_desc').val();
         const price = $('#new_product_price').val();
         const onStock = $('#new_product_stock').val();
         const productCategoryName = $('#new_product_cat').val();
@@ -22,38 +22,33 @@ $(document).ready(function () {
 
         var newProductObject = new Object();
 
-        newProductObject.title = title;
-        newProductObject.desc = desc;
-        newProductObject.price = price;
-        newProductObject.onStock = onStock;
-        //newProductObject.categoryid = 1;
+        newProductObject.name = name;
+        newProductObject.description = description;
+        newProductObject.price = parseInt(price);
+        newProductObject.product_stock = parseInt(onStock);
         newProductObject.category = productCategoryName;
 
         var productJSON = JSON.stringify(newProductObject);
 
+        console.log(productJSON);
+
         var jsonTest = ' {"category" : { "id" : 1, "name" : "Smartphones"}, "name" : "Iphone 16", "description" : "Description", "price" : 500 , "product_stock" : 10}';
-
-        console.log(newProductObject);
-
-
-        // INSERT INTO `product`(`id`, `description`, `name`, `price`, `product_stock`, `categoryid`)
-        // VALUES ('1','Description1','Iphone X','499.0','18','1'),
-
 
         var request = $.ajax({
             method: 'POST',
             url: 'http://localhost:8080/products/new',
-            data: newProductObject,
+            xhrFields:{
+                withCredentials:true
+            },
+            data: productJSON,
             headers: {
                 //Authorization: 'application/json'
                 'Content-Type': 'application/json',
             }
         });
 
-        console.log(newProductObject);
-
         request.done(function (msg) {
-            console.log('Success');
+            location.reload();
         });
 
         request.fail(function (jqXHR, textStatus) {
